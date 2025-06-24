2c409424a350881c8a5c6a798f0a8f924b0c8ef5
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2025-06-24 11:19:14 +0900

    feat: achievement_participant 모델에서 personal_achievement_id 필드 제거 및 마이그레이션 파일 추가

---
+++ prisma/migrations/20250623065735_fix_participant/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `personal_achievement_id` on the `achievement_participant` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "achievement_participant" DROP COLUMN "personal_achievement_id";
